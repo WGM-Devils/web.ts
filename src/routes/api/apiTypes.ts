c798bb765defb43fa821bc4d9083a81c699347e4
Leon™ <leontm.dev@gmail.com>
2023-11-07 16:20:38 +0100

    Added API Types

---
+++ src/routes/api/apiTypes.ts
@@ -0,0 +1,84 @@
+type Id = string;
+interface User {
+  id: Id;
+  username: string;
+  description: string;
+  pfp: {
+    extension: "png" | "jpg";
+    link: string;
+  };
+  banner: {
+    extension: "png" | "jpg";
+    link: string;
+  };
+  likes: {
+    count: number;
+    collection: Id[];
+  };
+  comments: {
+    count: number;
+    collection: Id[];
+  };
+  posts: {
+    count: number;
+    collection: Id[];
+  };
+  views: {
+    count: number;
+    collection: Id[];
+  };
+  events: Id[];
+  groups: Group[];
+}
+interface Group {
+  id: Id;
+  creator: Id;
+  name: string;
+  slogan: string;
+  members: Id[];
+}
+interface UserEvent {
+  id: Id[];
+  name: string;
+  description: string;
+  logo: {
+    type: "png" | "jpg";
+    link: string;
+  };
+  host: Id;
+  website: string;
+  date: string;
+}
+interface Post {
+  id: Id;
+  user: Id;
+  title: string;
+  description: string;
+  embed: {
+    type: "link" | "sound" | "picture" | "video";
+    link: string;
+  };
+  comments: {
+    count: number;
+    collection: Id[];
+  };
+  likes: {
+    count: number;
+    collection: Id[];
+  };
+  views: {
+    count: number;
+    collection: Id[];
+  };
+}
+interface UserComment {
+  id: Id;
+  creator: Id;
+  post: Id;
+}
+interface UserLike {
+  id: Id;
+  user: Id;
+  post: Id;
+}
+export { Id, User, Group, UserEvent, Post, UserComment, UserLike };
