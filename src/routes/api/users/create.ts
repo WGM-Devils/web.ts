8bc0e0dba17df963e13b72cf9bcb6ca77f2a1726
Leon™ <leontm.dev@gmail.com>
2023-11-07 16:50:29 +0100

    Added create Method for the user API

---
+++ src/routes/api/users/create.ts
@@ -0,0 +1,54 @@
+// Imports
+
+import fs from "fs-extra";
+import { Router } from "express";
+import response from "../errors/errors";
+import randomString from "random-string";
+import { Id, User, isUser } from "../apiTypes";
+
+// Presets
+
+let all: any = fs.readJsonSync("data/files/all.json");
+let users: object[] = fs.readJsonSync("data/files/users.json").users;
+
+// Code
+
+export default async function (router: Router) {
+  router.get("/", (req, res) => {
+    if (req.headers["authorization"] !== "") {
+      if (req.headers["authorization"] === process.env.KEY) {
+        let newUser = JSON.parse(req.body);
+        let id: Id = randomString({
+          length: 30,
+          numeric: true,
+          letters: false,
+          special: false,
+        });
+        while (all[id] != undefined) {
+          id = randomString({
+            length: 30,
+            numeric: true,
+            letters: false,
+            special: false,
+          });
+        }
+        newUser.id = id;
+        if (isUser(newUser)) {
+          try {
+            all[id] = "user";
+            users.push(newUser);
+            res.status(200).json(newUser);
+          } catch (error) {
+            res.status(500).json(response(500));
+          }
+        } else {
+          return res.status(409).json(response(409));
+        }
+      } else {
+        return res.status(401).json(response(401));
+      }
+    } else {
+      return res.status(400).json(response(400));
+    }
+  });
+}
