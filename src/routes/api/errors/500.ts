b07dd7991f4004350d7f1a0bf8beed1a14642e17
Leon™ <leontm.dev@gmail.com>
2023-11-07 16:44:12 +0100

    Created err500

---
+++ src/routes/api/errors/500.ts
@@ -0,0 +1,6 @@
+export default {
+  code: 500,
+  status: "Internal Server Error",
+  message:
+    "There was an unexpected server error, We are sorry for this instance, please try again in a few seconds.",
+};
