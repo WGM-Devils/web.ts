eb5feaf55aab4d366a37d3f82a90c6dfdf4245fb
LeonTM <LAnneken09@gmail.com>
2023-11-08 15:00:21 +0100

    Fixed user creation

--- src/routes/api/users/create.ts
+++ src/routes/api/users/create.ts
@@ -1,7 +1,7 @@
 // Imports
 
 import fs from "fs-extra";
-import { Router } from "express";
+import express from "express";
 import { response } from "../errors/errors";
 import randomString from "random-string";
 import { Id, User, isUser } from "../apiTypes";
@@ -9,46 +9,49 @@ import { Id, User, isUser } from "../apiTypes";
 // Presets
 
 let all: any = fs.readJsonSync("data/files/all.json");
-let users: object[] = fs.readJsonSync("data/files/users.json").users;
+let users: User[] = fs.readJsonSync("data/files/users.json").users;
+let createRouter = express.Router();
 
 // Code
 
-export default async function (router: Router) {
-  router.get("/", (req, res) => {
-    if (req.headers["authorization"] !== "") {
-      if (req.headers["authorization"] === process.env.KEY) {
-        let newUser = JSON.parse(req.body);
-        let id: Id = randomString({
+createRouter.get("/", (req, res) => {
+  if (req.headers["authorization"] !== "") {
+    if (req.headers["authorization"] === process.env.KEY) {
+      let newUser = JSON.parse(req.body);
+      let id: Id = randomString({
+        length: 30,
+        numeric: true,
+        letters: false,
+        special: false,
+      });
+      while (all[id] != undefined) {
+        id = randomString({
           length: 30,
           numeric: true,
           letters: false,
           special: false,
         });
-        while (all[id] != undefined) {
-          id = randomString({
-            length: 30,
-            numeric: true,
-            letters: false,
-            special: false,
-          });
-        }
-        newUser.id = id;
-        if (isUser(newUser)) {
-          try {
-            all[id] = "user";
-            users.push(newUser);
-            res.status(200).json(newUser);
-          } catch (error) {
-            res.status(500).json(response(500));
-          }
-        } else {
-          return res.status(409).json(response(409));
+      }
+      newUser.id = id;
+      if (isUser(newUser)) {
+        try {
+          all[id] = "user";
+          users.push(newUser);
+          res.status(200).json(newUser);
+        } catch (error) {
+          res.status(500).json(response(500));
         }
       } else {
-        return res.status(401).json(response(401));
+        return res.status(409).json(response(409));
       }
     } else {
-      return res.status(400).json(response(400));
+      return res.status(401).json(response(401));
     }
-  });
-}
+  } else {
+    return res.status(400).json(response(400));
+  }
+});
+
+// Exports
+
+export default createRouter;
