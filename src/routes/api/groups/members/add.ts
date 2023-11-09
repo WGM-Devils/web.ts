e7f6938bbf955eee9fad0404e942c595f1f76571
LeonTM <LAnneken09@gmail.com>
2023-11-09 09:05:16 +0100

    Added main setup for add members method for the groups api

---
+++ src/routes/api/groups/members/add.ts
@@ -0,0 +1,16 @@
+// Imports
+
+import express from "express";
+import fs from "fs-extra";
+
+// Presets
+
+let addRouter = express.Router();
+
+// Code
+
+addRouter.get("/", (req, res) => {});
+
+// Exports
+
+export default addRouter;
