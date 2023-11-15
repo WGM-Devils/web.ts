4559cb035d7bc7a28f92d111c4e102349d62fd3c
LeonTM <LAnneken09@gmail.com>
2023-11-15 14:51:01 +0100

    Finished users api main file

--- src/routes/api/users/users.ts
+++ src/routes/api/users/users.ts
@@ -6,15 +6,23 @@ import express from "express";
 
 let usersRouter = express.Router();
 
-// API Imports
+// API-Imports
 
 import allRouter from "./all";
 import createRouter from "./create";
+import deleteRouter from "./delete";
+import resetRouter from "./reset";
+import editRouter from "./edit";
+import getRouter from "./get";
 
 // Code
 
 usersRouter.use("/all", allRouter);
 usersRouter.use("/create", createRouter);
+usersRouter.use("/delete", deleteRouter);
+usersRouter.use("/reset", resetRouter);
+usersRouter.use("/edit", editRouter);
+usersRouter.use("/get", getRouter);
 
 // Exports
 
