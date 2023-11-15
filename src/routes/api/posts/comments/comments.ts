93163e72a9acec91ca0a3aa0c8fa6a085dd42547
LeonTM <LAnneken09@gmail.com>
2023-11-15 14:30:49 +0100

    Set up the comments api for posts

--- src/routes/api/posts/comments/comments.ts
+++ src/routes/api/posts/comments/comments.ts
@@ -1,7 +1,14 @@
 // Imports
 
 import express, { Router } from "express";
+
+// API-Imports
+
 import sendRouter from "./send";
+import editRouter from "./enable";
+import deleteRouter from "./delete";
+import disableRouter from "./disable";
+import enableRouter from "./enable";
 
 // Presets
 
@@ -10,6 +17,10 @@ let commentsRouter = express.Router();
 // Code
 
 commentsRouter.use("/send", sendRouter);
+commentsRouter.use("/edit", editRouter);
+commentsRouter.use("/delete", deleteRouter);
+commentsRouter.use("/disable", disableRouter);
+commentsRouter.use("/enable", enableRouter);
 
 // Exports
 
