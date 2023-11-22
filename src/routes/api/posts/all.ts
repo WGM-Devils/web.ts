530df70080fcae108979f96848c913725aa91477
LeonTM <LAnneken09@gmail.com>
2023-11-22 14:33:42 +0100

    Added method of getting all posts

--- src/routes/api/posts/all.ts
+++ src/routes/api/posts/all.ts
@@ -1,7 +1,7 @@
 // Imports
 
-import express from "express";
 import fs from "fs-extra";
+import express from "express";
 import { response } from "../errors/errors";
 
 // Presets
@@ -11,8 +11,10 @@ let allRouter = express.Router();
 // Code
 
 allRouter.get("/", (req, res) => {
-  if (req.headers["authorization"] != undefined) {
+  if (req.headers["authorization"] !== "") {
     if (req.headers["authorization"] === process.env.KEY) {
+      let posts = fs.readJsonSync("src/data/files/posts.json").posts;
+      return res.status(200).json({ posts: posts });
     } else {
       return res.status(401).json(response(401));
     }
