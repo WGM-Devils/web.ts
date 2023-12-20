fffe86f6b304799a8bf9bafbc90b320b16219ebc
LeonTM <LAnneken09@gmail.com>
2023-12-20 13:44:18 +0100

    ole

--- src/routes/api/posts/delete.ts
+++ src/routes/api/posts/delete.ts
@@ -3,16 +3,27 @@
 import express from "express";
 import fs from "fs-extra";
 import { response } from "../errors/errors";
+import { Post, User, UserComment } from "../apiTypes";
 
 // Presets
 
 let deleteRouter = express.Router();
+let all = fs.readJsonSync("src/data/files/all.json");
+let posts: Post[] = fs.readJsonSync("src/data/files/posts.json").posts;
+let users: User[] = fs.readJsonSync("src/data/files/users.json").users;
+let comments: UserComment[] = fs.readJsonSync(
+  "src/data/files/comments.json"
+).comments;
 
 // Code
 
-deleteRouter.delete("/", (req, res) => {
+deleteRouter.delete("/postId=:post", (req, res) => {
   if (req.headers["authorization"] != undefined) {
     if (req.headers["authorization"] === process.env.KEY) {
+      if (all[req.params.post]) {
+      } else {
+        return res.status(404).json(response(404));
+      }
     } else {
       return res.status(401).json(response(401));
     }
