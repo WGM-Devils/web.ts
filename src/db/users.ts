44eb82fa472e40bd188258f5d7743df3a1991614
Leon™ <leontm.dev@gmail.com>
2023-12-20 16:46:42 +0100

    New packages lodash

--- src/db/users.ts
+++ src/db/users.ts
@@ -40,9 +40,15 @@ const UserSchema = new mongoose.Schema({
 
 export const UserModel = mongoose.model("User", UserSchema, "");
 
+export const getUsers = () => UserModel.find();
+
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({ "auth.sessionToken": sessionToken });
 export const getUserById = (id: string) => UserModel.findById(id);
-export const getAllUsers = () => UserModel.find();
-export const deleteUser = (id: string) =>
-  UserModel.findOneAndDelete({ _id: id });
 export const createUser = (values: Record<string, any>) =>
   new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (id: string) =>
+  UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) =>
+  UserModel.findByIdAndUpdate(id, values);
