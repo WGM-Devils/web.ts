4eeb9f7ec024daa81ab4bba8421640ae0a4904cd
Leon™ <leontm.dev@gmail.com>
2023-12-20 16:47:01 +0100

    Updated user db

--- src/db/users.ts
+++ src/db/users.ts
@@ -44,7 +44,7 @@ export const getUsers = () => UserModel.find();
 
 export const getUserByEmail = (email: string) => UserModel.findOne({ email });
 export const getUserBySessionToken = (sessionToken: string) =>
-  UserModel.findOne({ "auth.sessionToken": sessionToken });
+  UserModel.findOne({ "authentication.sessionToken": sessionToken });
 export const getUserById = (id: string) => UserModel.findById(id);
 export const createUser = (values: Record<string, any>) =>
   new UserModel(values).save().then((user) => user.toObject());
