00735a6c95ae166142817043d31ccfdf4a4ae65c
Leon™ <leontm.dev@gmail.com>
2023-12-21 17:31:48 +0100

    Added get method to the router and added type variation to every request

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,10 +1,26 @@
 import express from "express";
 
-import { deleteUser, getAllUsers, updateUser } from "../controller/users";
+import {
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from "../controller/users";
 import { isAuthenticated, isOwner } from "../middlewares";
 
 export default (router: express.Router) => {
-  router.get("/users", isAuthenticated, getAllUsers);
-  router.delete("/users/:id", isAuthenticated, isOwner, deleteUser);
-  router.patch("/users/:id", isAuthenticated, isOwner, updateUser);
+  router.get("/users/all/type=:type", getAllUsers);
+  router.get("/users/get/id=:id/type=:type", isAuthenticated, getUser);
+  router.delete(
+    "/users/delete/id=:id/type=:type",
+    isAuthenticated,
+    isOwner,
+    deleteUser
+  );
+  router.patch(
+    "/users/update/id=:id/type=:type",
+    isAuthenticated,
+    isOwner,
+    updateUser
+  );
 };
