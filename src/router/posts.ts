ae3a04dbf8e2ad85e5a36d11e011140fbfe18e4e
Leon™ <leontm.dev@gmail.com>
2024-01-08 18:09:00 +0100

    Removed type

--- src/router/posts.ts
+++ src/router/posts.ts
@@ -14,7 +14,7 @@ import {
 // Exports
 
 export default (router: express.Router) => {
-  router.get("/posts/all/type=:type", getAllPosts);
+  router.get("/posts/all", getAllPosts);
   router.get("/posts/get/id=:id/type=:type", getPost);
   router.delete("/posts/delete/postId=:postId", deletePost);
   router.patch("/posts/update/id=:id/type=:type", updatePost);
