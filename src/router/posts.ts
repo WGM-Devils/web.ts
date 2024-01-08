4b832c0715c4d559e0f88fa5dda624d799d61661
Leon™ <leontm.dev@gmail.com>
2024-01-08 17:36:47 +0100

    finished post router

--- src/router/posts.ts
+++ src/router/posts.ts
@@ -6,22 +6,10 @@ import {
   getPost,
   updatePost,
 } from "../controller/posts";
-import { isAuthenticated } from "../middlewares/users";
-import { isCreator } from "../middlewares/posts";
 
 export default (router: express.Router) => {
   router.get("/posts/all/type=:type", getAllPosts);
-  router.get("/posts/get/id=:id/type=:type", isAuthenticated, getPost);
-  router.delete(
-    "/posts/delete/postId=:postId/type=:type",
-    isAuthenticated,
-    isCreator,
-    deletePost
-  );
-  router.patch(
-    "/posts/update/id=:id/type=:type",
-    isAuthenticated,
-    isCreator,
-    updatePost
-  );
+  router.get("/posts/get/id=:id/type=:type", getPost);
+  router.delete("/posts/delete/postId=:postId/type=:type", deletePost);
+  router.patch("/posts/update/id=:id/type=:type", updatePost);
 };
