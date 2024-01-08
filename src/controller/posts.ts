4af139b5aa59410e5d792e63a3712a0979f034c8
Leon™ <leontm.dev@gmail.com>
2024-01-08 17:35:19 +0100

    Finished the post controller

--- src/controller/posts.ts
+++ src/controller/posts.ts
@@ -1,26 +1,49 @@
 import express from "express";
 
-import { deleteUserById, getUserById, getPosts, createPost } from "../db/posts";
+import { deleteById, updateById, getById, getAll, create } from "../db/posts";
+import { sendAPIResponse } from "helpers/respond";
 
-export const getAllUsers = async (
+export const getAllPosts = async (
   req: express.Request,
   res: express.Response
 ) => {
   try {
     const { type } = req.params;
-    const users = await getUsers();
+    const posts = await getAll();
 
     if (type === "json") {
-      return res.status(200).json(users).end();
+      return res
+        .status(200)
+        .json(
+          sendAPIResponse(
+            200,
+            "Enjoy the contents you requested",
+            posts,
+            "json"
+          )
+        )
+        .end();
     } else {
       return res
         .status(200)
-        .json({ users: Object.values(users) })
+        .json(
+          sendAPIResponse(
+            200,
+            "Enjoy the contents you requested",
+            {
+              posts: Object.values(posts),
+            },
+            "arr"
+          )
+        )
         .end();
     }
   } catch (error) {
     console.log(error);
-    return res.sendStatus(400);
+    return res
+      .status(500)
+      .json(sendAPIResponse(500, "Our fault.", null, null))
+      .end();
   }
 };
 
@@ -29,94 +52,179 @@ export const createPost = async (
   res: express.Response
 ) => {
   try {
-    const { title, content, embed } = req.body;
-    const { sessionToken } = req.cookies;
-    const user = await getUserBySessionToken(sessionToken);
+    const { title, content, embed, user } = req.body;
 
     if (!user) {
-      return res.sendStatus(400);
+      return res
+        .status(400)
+        .json(sendAPIResponse(400, "You need a user!", null, null))
+        .end();
     }
 
     if (!title || !content || !embed) {
-      return res.sendStatus(400);
+      return res
+        .status(400)
+        .json(
+          sendAPIResponse(
+            400,
+            "Check your request once more please.",
+            null,
+            null
+          )
+        )
+        .end();
     }
 
-    const post = await createPost();
+    const post = await create({
+      user: user,
+      title: title,
+      content: content,
+      embed: embed,
+    });
+
+    if (!post) {
+      return res
+        .status(400)
+        .json(
+          sendAPIResponse(400, "Check your request body once more.", null, null)
+        )
+        .end();
+    }
+
+    return res
+      .status(201)
+      .json(
+        sendAPIResponse(
+          201,
+          "Post created.",
+          { posts: Object.values(post) },
+          "arr"
+        )
+      )
+      .end();
   } catch (error) {
     console.log(error);
-    return res.sendStatus(400);
+    return res
+      .status(500)
+      .json(sendAPIResponse(500, "We ran into an issue.", null, null));
   }
 };
 
-export const deleteUser = async (
+export const deletePost = async (
   req: express.Request,
   res: express.Response
 ) => {
   try {
     const { id, type } = req.params;
 
-    const deletedUser = await deleteUserById(id);
+    const deletedPost = await deleteById(id);
 
-    if (type === "json") {
-      return res.status(200).json(deletedUser).end();
-    } else {
-      return res
-        .status(200)
-        .json({ users: Object.values(deletedUser) })
-        .end();
-    }
+    return res
+      .status(201)
+      .json(sendAPIResponse(201, "Deleted your post.", null, null))
+      .end();
   } catch (error) {
     console.log(error);
-    return res.sendStatus(400);
+    return res
+      .status(500)
+      .json(sendAPIResponse(500, "We ran into an issue.", null, null));
   }
 };
 
-export const getUser = async (req: express.Request, res: express.Response) => {
+export const getPost = async (req: express.Request, res: express.Response) => {
   try {
     const { id, type } = req.params;
 
-    const user = await getUserById(id);
+    const post = await getById(id);
 
-    if (!user) {
-      return res.sendStatus(404);
+    if (!post) {
+      return res
+        .status(404)
+        .json(
+          sendAPIResponse(
+            404,
+            "There is no post connected to the given id",
+            null,
+            null
+          )
+        );
     }
 
     if (type === "json") {
-      return res.status(200).json(user).end();
+      return res
+        .status(200)
+        .json(sendAPIResponse(200, "Your requested resource.", post, "json"))
+        .end();
     } else {
       return res
         .status(200)
-        .json({ users: Object.values(user) })
+        .json(
+          sendAPIResponse(
+            200,
+            "Your requested resource.",
+            { posts: Object.values(post) },
+            "arr"
+          )
+        )
         .end();
     }
   } catch (error) {
     console.log(error);
-    return res.sendStatus(400);
+    return res.status(500).json(sendAPIResponse(500, "Our bad.", null, null));
   }
 };
 
-export const updateUser = async (
+export const updatePost = async (
   req: express.Request,
   res: express.Response
 ) => {
   try {
     const { id, type } = req.params;
-    const { username } = req.body;
+    const { title, content, embed } = req.body;
 
-    if (!username) {
-      return res.sendStatus(403);
+    const post = await getById(id);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json(sendAPIResponse(404, "There is no post.", null, null))
+        .end();
     }
 
-    const user = await getUserById(id);
-    user.username = username;
-    await user.save();
+    if (!title || !content || !embed) {
+      return res
+        .status(400)
+        .json(
+          sendAPIResponse(
+            400,
+            "Please give all the necessary values.",
+            null,
+            null
+          )
+        );
+    }
+
+    post.title = title;
+    post.content = content;
+    post.embed = embed;
+    await post.save();
 
     if (type === "json") {
-      return res.status(200).json(user).end();
+      return res
+        .status(200)
+        .json(sendAPIResponse(200, "Your updated post.", post, "json"))
+        .end();
     } else {
       return res
         .status(200)
-        .json({ users: Object.values(user) })
+        .json(
+          sendAPIResponse(
+            200,
+            "Your updated post.",
+            { posts: Object.values(post) },
+            "arr"
+          )
+        )
         .end();
     }
   } catch (error) {
