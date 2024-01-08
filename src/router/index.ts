e64e20e94796e2d8503565ef3c22e4703b0b0b26
Leon™ <leontm.dev@gmail.com>
2024-01-08 18:40:09 +0100

    Added new router

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,8 @@ import express from "express";
 
 import auth from "./auth";
 import users from "./users";
+import groups from "./groups";
+import posts from "./posts";
 
 // Constants
 
@@ -16,5 +18,7 @@ const router = express.Router();
 export default (): express.Router => {
   users(router);
   auth(router);
+  posts(router);
+  groups(router);
   return router;
 };
