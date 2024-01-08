bafcf0be84d35ddca541aa462a4ab7afb9846779
Leon™ <leontm.dev@gmail.com>
2024-01-08 18:17:25 +0100

    Added a schema for groups

---
+++ src/db/groups.ts
@@ -0,0 +1,30 @@
+// Imports
+
+import mongoose from "mongoose";
+
+// Code
+
+const GroupSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  creator: { type: String, required: true },
+  description: { type: String, required: true },
+  members: { type: Array, default: [] },
+  slogan: { type: String, required: false },
+  city: {
+    latitude: { type: String, required: false },
+    longitude: { type: String, required: false },
+  },
+});
+
+// Exports
+
+export const GroupModel = mongoose.model("Group", GroupSchema, "groups");
+
+export const getAll = async () => GroupModel.find();
+export const getById = async (id: string) => GroupModel.findById(id);
+export const getByCreator = async (creator: string) =>
+  GroupModel.find({ creator: creator });
+export const deleteById = async (id: string) =>
+  GroupModel.findByIdAndDelete(id);
+export const updateById = async (id: string, values: Record<string, any>) =>
+  GroupModel.findByIdAndUpdate(id, values);
