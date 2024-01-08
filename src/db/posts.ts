a66f30b08007c8be57918f14f632f5034ca00b91
Leon™ <leontm.dev@gmail.com>
2024-01-08 16:54:42 +0100

    Updated the post database model

--- src/db/posts.ts
+++ src/db/posts.ts
@@ -1,26 +1,44 @@
+// Imports
+
 import mongoose from "mongoose";
+
+// Code
 
 const PostSchema = new mongoose.Schema({
   user: { type: String, required: true },
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  content: { type: String, required: true },
   embed: {
     type: { type: String, required: false },
     link: { type: String, required: false },
   },
   comments: {
-    allowed: { type: Boolean, required: true },
-    count: { type: Number, required: true },
-    collection: { type: Array, required: true },
+    allowed: { type: Boolean, default: true },
+    count: { type: Number, default: 0 },
+    collection: { type: Array, default: [] },
   },
   likes: {
-    count: { type: Number, required: true },
-    collection: { type: Array, required: true },
+    count: { type: Number, default: 0 },
+    collection: { type: Array, default: [] },
   },
   views: {
-    count: { type: Number, required: true },
-    collection: { type: Array, required: true },
+    count: { type: Number, default: 0 },
+    collection: { type: Array, default: [] },
   },
-  createdAt: { type: Date, required: true },
-  lastUpdated: { type: Date, required: true },
+  createdAt: { type: Date, default: Date.now() },
+  lastUpdated: { type: Date, default: Date.now() },
 });
+
+const PostModel = mongoose.model("Post", PostSchema, "posts");
+
+// Exports
+
+export const getPosts = () => PostModel.find();
+
+export const getPostById = (id: string) => PostModel.findById(id);
+export const createPost = (values: Record<string, any>) =>
+  new PostModel(values).save().then((post) => post.toObject());
+export const deletePostById = (id: string) =>
+  PostModel.findOneAndDelete({ _id: id });
+export const updatePostById = (id: string, values: Record<string, any>) =>
+  PostModel.findByIdAndUpdate(id, values);
