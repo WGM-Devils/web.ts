f148cbb10539c3887c10ab671488bec5a325f5c4
Leon™ <leontm.dev@gmail.com>
2024-01-09 16:08:34 +0100

    Added new messages to post router

--- src/router/posts.ts
+++ src/router/posts.ts
@@ -9,6 +9,10 @@ import {
   getAllPosts,
   getPost,
   updatePost,
+  createPost,
+  likePost,
+  unlikePost,
+  viewedPost,
 } from "../controller/posts";
 
 // Exports
@@ -16,6 +20,10 @@ import {
 export default (router: express.Router) => {
   router.get("/posts/all", getAllPosts);
   router.get("/posts/get/id=:id/type=:type", getPost);
-  router.delete("/posts/delete/postId=:postId", deletePost);
+  router.delete("/posts/delete/postId=:postId/userId=:userId", deletePost);
   router.patch("/posts/update/id=:id/type=:type", updatePost);
+  router.post("/posts/create", createPost);
+  router.post("/posts/like/postId=:postId/userId=:userId", likePost);
+  router.delete("/posts/unlike/postId=:postId/userId=:userId", unlikePost);
+  router.post("/posts/viewed/postId=:postId/userId=:userId", viewedPost);
 };
