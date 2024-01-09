fb0993d0d42d74090b9e31589762c73b9107b951
LeonTM <LAnneken09@gmail.com>
2024-01-09 09:08:08 +0100

    Finished userEvents router

---
+++ src/router/userEvents.ts
@@ -0,0 +1,25 @@
+// Imports
+
+import express from "express";
+
+// Project-Imports
+
+import {
+  getAllUserEvents,
+  getAllUserEventsByCreator,
+  getUserEvent,
+  deleteUserEvent,
+  updateUserEvent,
+  createUserEvent,
+} from "controller/userEvent";
+
+// Code + Exports
+
+export default (router: express.Router) => {
+  router.get("/userEvents/all", getAllUserEvents);
+  router.get("/userEvents/all/creator=:creator", getAllUserEventsByCreator);
+  router.get("/userEvents/get/id=:id/type=:type", getUserEvent);
+  router.delete("/userEvents/delete/id=:id", deleteUserEvent);
+  router.patch("/userEvents/update/id=:id/type=:type", updateUserEvent);
+  router.post("/userEvents/create", createUserEvent);
+};
